eae71f723fbcf7758c658d4ef655c88a79589c6e
keitango <godspeed4246@gmail.com>
2018-07-26 07:12:19 +0900

    add jscafe_http.js

---
+++ module/jscafe_http.js
@@ -0,0 +1,26 @@
+var http = require("http");
+
+var JSCafe = {
+  request : function(url, callback){
+    http.get(url,function(res)){
+      res.setEncoding('utf-8');
+      var buffer = '';
+      res.on('readable', function(){
+        buffer += res.read();
+      });
+      res.on('end', function(){
+        // コールバック呼び出し部
+        // レスポンスがすべて帰ってきた場合にコールバックを呼び出し，内容を第二引数に入れる
+        callback(null, buffer);
+      });
+      res.on('error', function(){
+        // エラーが発生した場合エラーメッセージを受け取り，コールバックの第一引数にエラーを入れる
+        // 慣例的にエラーを返すメソッドの場合，第一引数をエラーにすることが多い
+        console.log('Got error : ' + e.message);
+        callback(e, buffer);
+      });
+    });
+  }
+};
+
+module.export = JScafe;  // 指定されたURLページをコールバックで取得する
