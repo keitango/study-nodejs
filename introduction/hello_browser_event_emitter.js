5d4e651e904df560958a5914051fdafbf260c06e
keitango <godspeed4246@gmail.com>
2018-07-25 21:50:52 +0900

    add hello_browser_event_emitter.js

---
+++ introduction/hello_browser_event_emitter.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+  res.end("hello nodejs");
+});
+
+// listeningイベントを受け取り表示する
+server.on('listening', function(){
+  console.log("Server started, listening on : 30000");
+});
+
+server.listen(30000);
